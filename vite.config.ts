e1a6cf82c0548fd371985bbd5449be169d049575
kokuyouwind <kokuyouwind@gmail.com>
2022-12-05 23:30:57 +0900

    add some configs
    @see https://zenn.dev/sprout2000/articles/98145cf2a807b1

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  root: './src',
+  publicDir: '../public',
+  build: {
+    // root (= ./src) から見た相対パスで指定
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+  server: {
+    open: true,
+  },
   plugins: [react()]
 })
