4aeba673ee2015bc445fefe041993ae99d7956e2
kokuyouwind <kokuyouwind@gmail.com>
2022-12-05 23:38:58 +0900

    add eslint
    @see https://zenn.dev/big_tanukiudon/articles/c1ab3dba7ba111#eslint-%E5%B0%8E%E5%85%A5-%EF%BC%86-%E5%AE%9F%E8%A1%8C
    @see https://zenn.dev/longbridge/articles/ae3aa36cf17d73#eslint%E3%81%AE%E5%B0%8E%E5%85%A5

---
+++ .eslintrc.cjs
@@ -0,0 +1,33 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'standard-with-typescript'
+  ],
+  overrides: [
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: [
+    'react',
+    'react-hooks',
+    '@typescript-eslint',
+  ],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
