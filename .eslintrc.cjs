fbf0c73a93d623de16447c4ee242788ec20ea4d5
kokuyouwind <kokuyouwind@gmail.com>
2022-12-07 08:42:47 +0900

    add storybook

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,32 +4,22 @@ module.exports = {
     es2021: true,
     node: true
   },
-  extends: [
-    'plugin:react/recommended',
-    'plugin:react-hooks/recommended',
-    'standard-with-typescript',
-    "prettier",
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', 'standard-with-typescript', 'prettier', 'plugin:storybook/recommended'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
-    project: './tsconfig.json',
+    project: './tsconfig.json'
   },
-  plugins: [
-    'react',
-    'react-hooks',
-    '@typescript-eslint',
-  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   rules: {
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': 'warn',
-    '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/triple-slash-reference': 'off'
   },
   settings: {
     react: {
-      version: 'detect',
-    },
-  },
-}
+      version: 'detect'
+    }
+  }
+};
