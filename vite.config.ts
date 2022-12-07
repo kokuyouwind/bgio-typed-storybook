9ef157c403445207001b6b3df4ef35a3ecae8776
kokuyouwind <kokuyouwind@gmail.com>
2022-12-07 21:56:58 +0900

    change directory structure for boardgame.io

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: './',
-  root: './src',
-  publicDir: '../public',
+  root: './src/client',
+  publicDir: './public',
   build: {
     // root (= ./src) から見た相対パスで指定
-    outDir: '../dist',
+    outDir: '../../dist/client',
     emptyOutDir: true,
   },
   server: {
