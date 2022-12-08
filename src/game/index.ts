a1b226822e4f4da1287fc3aeb5b41e39d4566fa1
kokuyouwind <kokuyouwind@gmail.com>
2022-12-08 22:01:31 +0900

    set min/max moves

--- src/game/index.ts
+++ src/game/index.ts
@@ -3,6 +3,10 @@ import moves from './moves'
 
 const TicTacToe: Game = {
   setup: () => ({ cells: Array(9).fill(null) }),
+  turn: {
+    minMoves: 1,
+    maxMoves: 1,
+  },
   moves,
 }
 export default TicTacToe
