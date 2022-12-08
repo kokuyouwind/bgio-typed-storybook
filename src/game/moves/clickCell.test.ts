324f9938b5f2f2cd10e2452656f2434b27c10353
kokuyouwind <kokuyouwind@gmail.com>
2022-12-08 22:00:56 +0900

    check invalid move

--- src/game/moves/clickCell.test.ts
+++ src/game/moves/clickCell.test.ts
@@ -1,17 +1,28 @@
 import { describe, test, expect } from 'vitest'
 import clickCell from './clickCell'
+import {INVALID_MOVE} from "boardgame.io/core";
 
 describe('suite', () => {
   test('should place the correct value in the cell', () => {
-    // original state.
     const G = {
       cells: [null, null, null, null, null, null, null, null, null],
     }
 
-    // make move.
     clickCell({ G, playerID: '1' }, 3)
 
-    // verify new state.
+    expect(G).toEqual({
+      cells: [null, null, null, '1', null, null, null, null, null],
+    })
+  })
+
+  test('place only empty cell', () => {
+    const G = {
+      cells: [null, null, null, '1', null, null, null, null, null],
+    }
+
+    clickCell({ G, playerID: '2' }, 3)
+
+    // not changed
     expect(G).toEqual({
       cells: [null, null, null, '1', null, null, null, null, null],
     })
--- src/game/moves/clickCell.ts
+++ src/game/moves/clickCell.ts
@@ -1,9 +1,13 @@
 import { MoveContext, MoveResult } from '../types'
+import {INVALID_MOVE} from "boardgame.io/core";
 
 const clickCell = (
   { G, playerID }: Pick<MoveContext, 'G' | 'playerID'>,
   id: number
 ): MoveResult => {
+  if (G.cells[id] !== null) {
+    return INVALID_MOVE;
+  }
   G.cells[id] = playerID
 }
 export default clickCell
