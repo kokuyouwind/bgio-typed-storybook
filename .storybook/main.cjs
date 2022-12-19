519371cd24aefb21619ae594806f91679b41fffd
kokuyouwind <kokuyouwind@gmail.com>
2022-12-19 16:46:24 +0900

    adjust path settings

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,7 +1,7 @@
 module.exports = {
   "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    "../src/client/components/**/*.stories.mdx",
+    "../src/client/components/**/*.stories.@(js|jsx|ts|tsx)"
   ],
   "addons": [
     "@storybook/addon-links",
