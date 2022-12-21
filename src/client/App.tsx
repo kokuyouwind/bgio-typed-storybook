239306ae3e20b4d22bb6ae953665fcd6ff2b33c1
kokuyouwind <kokuyouwind@gmail.com>
2022-12-22 08:53:22 +0900

    render board

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -1,6 +1,14 @@
-import { Client } from 'boardgame.io/react'
+import { Client, BoardProps } from 'boardgame.io/react'
+import { GameState } from 'game/types'
+import React from 'react'
 import TicTacToe from '../game'
+import { Board } from './components/boards/Board'
 
-const App = Client({ game: TicTacToe })
+type Props = BoardProps<GameState>
+const TicTacToeBoard: React.FC<Props> = ({ G }) => {
+  return <Board board={G.board} />
+}
+
+const App = Client({ game: TicTacToe, board: TicTacToeBoard })
 
 export default App
