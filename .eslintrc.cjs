c2d6ba541c3f9cd0493215c11e498694e3fa5653
kokuyouwind <kokuyouwind@gmail.com>
2023-01-09 22:20:24 +0900

    disable rule @typescript-eslint/no-confusing-void-expression
    storybook interaction test requires await for void function call.

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -19,6 +19,7 @@ module.exports = {
     "react/react-in-jsx-scope": "off",
     "react/prop-types": "off",
     '@typescript-eslint/triple-slash-reference': 'off',
+    '@typescript-eslint/no-confusing-void-expression': 'off',
     'import/order': [
       'error',
       {
