23dc33f4f8338933b3d221847fc3f56ad17da77c
kokuyouwind <kokuyouwind@gmail.com>
2023-01-10 21:34:36 +0900

    split lobby directory

--- games/ticTacToe/src/client/App.tsx
+++ games/ticTacToe/src/client/App.tsx
@@ -1,15 +1,3 @@
-import React from 'react'
-import { Lobby } from 'boardgame.io/react'
-import TicTacToe from '../core'
-import MainView from './components/ui/MainView'
+import Client from './components/integrated/Client'
 
-const Client: React.FC = () => {
-  return (
-    <Lobby
-      gameServer={`http://${window.location.hostname}:8000`}
-      lobbyServer={`http://${window.location.hostname}:8000`}
-      gameComponents={[{ game: TicTacToe, board: MainView }]}
-    />
-  )
-}
 export default Client
